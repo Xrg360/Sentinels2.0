8c1c8c7340518cfcc0c6fe2daa00d6b8aaa02650
rohit george <code.mastermind.07@gmail.com>
2023-04-29 02:05:12 +0530

    added login ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
+    <>
     <Navbar/>
+    <MainPage/>
+    </>
+    
   );
 }
 
